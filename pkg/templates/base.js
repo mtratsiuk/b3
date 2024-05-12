d40880856fdae02678181e263ce032d72b40af36
Misha Tratsiuk <misha+git@spris.dev>
2024-05-12 17:39:55 +0200

    Add theme persistence

--- pkg/templates/base.js
+++ pkg/templates/base.js
@@ -1,14 +1,46 @@
 (function () {
   var DARK_THEME_CLASSNAME = "dark";
+  var THEME_STORAGE_KEY = "b3-theme"
+  var THEMES = {
+    JEDI: "jedi",
+    SITH: "sith",
+  }
 
-  var themeToggle = document.getElementById("theme-toggle");
-  var state = themeToggle.textContent
+  var state = tryExec(function () {
+    return window.localStorage.getItem(THEME_STORAGE_KEY)
+  }, THEMES.JEDI)
 
-  themeToggle.addEventListener("click", function () {
-    document.documentElement.classList.toggle(DARK_THEME_CLASSNAME);
+  if (state === THEMES.SITH && !document.documentElement.classList.contains(DARK_THEME_CLASSNAME)) {
+    document.documentElement.classList.add(DARK_THEME_CLASSNAME);
+  }
 
-    state = state === "jedi" ? "sith" : "jedi"
+  document.addEventListener('DOMContentLoaded', function () {
+    var themeToggle = document.getElementById("theme-toggle");
 
-    themeToggle.textContent = state
-  });
+    if (state === THEMES.SITH && themeToggle.textContent !== THEMES.SITH) {
+      themeToggle.textContent = THEMES.SITH
+    }
+
+    themeToggle.addEventListener("click", function () {
+      if (state === THEMES.JEDI) {
+        state = THEMES.SITH
+        document.documentElement.classList.add(DARK_THEME_CLASSNAME);
+      } else {
+        state = THEMES.JEDI
+        document.documentElement.classList.remove(DARK_THEME_CLASSNAME);
+      }
+
+      themeToggle.textContent = state
+      tryExec(function () { window.localStorage.setItem(THEME_STORAGE_KEY, state) })
+    });
+  })
+
+  function tryExec(fn, fallback) {
+    try {
+      return fn() || fallback
+    } catch (err) {
+      console.warn(err)
+      return fallback
+    }
+  }
 })();
